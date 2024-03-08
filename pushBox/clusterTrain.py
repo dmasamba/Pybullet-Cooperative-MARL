3bef44cd16908edd58b373285d142138b2172aaf
Daniel Masamba <dmasamba.dmn@gmail.com>
2024-03-08 16:13:05 -0600

    updated clusterTrain

--- pushBox/clusterTrain.py
+++ pushBox/clusterTrain.py
@@ -33,8 +33,8 @@ def linear_lr(progress_remaining: float):
 model = PPO('MlpPolicy', env, learning_rate=linear_lr, verbose=1, tensorboard_log=log_path)
 
 # train the model
-model.learn(total_timesteps=10000, callback=eval_callback)
+model.learn(total_timesteps=10000000, callback=eval_callback)
 
-PPO_Path = os.path.join('Training', 'clusterResults', 'clusterSavedModels', 'model_10M_15dgr_cylinder')
+PPO_Path = os.path.join('Training', 'clusterResults', 'clusterSavedModels', 'PPO_3_10M_15dgr_cylinder')
 
 model.save(PPO_Path)
