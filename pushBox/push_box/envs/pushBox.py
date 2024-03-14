35e1edc1591c5558a969d7fa63ccafcb0ff1c788
Daniel Masamba <dmasamba.dmn@gmail.com>
2024-03-14 13:30:27 -0500

    last updates

--- pushBox/push_box/envs/pushBox.py
+++ pushBox/push_box/envs/pushBox.py
@@ -26,7 +26,7 @@ class PushBoxEnv(gymnasium.Env):
         # self.action_space = spaces.Discrete(5)
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(10,), dtype=np.float64)
 
-        p.connect(p.DIRECT) #or p.DIRECT for non-graphical version
+        p.connect(p.GUI) #or p.DIRECT for non-graphical version
         p.resetSimulation()
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0,0,-9.8)
@@ -82,7 +82,7 @@ class PushBoxEnv(gymnasium.Env):
 
         self.state = self.init_state()
         self.step_count = 0
-        self.maxSteps = 300
+        self.maxSteps = 500
     
     def init_state(self):
 
@@ -128,7 +128,7 @@ class PushBoxEnv(gymnasium.Env):
         return qx, qy
     
     def step(self, action):
-        # time.sleep(1/60)
+        time.sleep(1/60)
         self.step_count += 1
         done = False
         #get agent, box and target positions
